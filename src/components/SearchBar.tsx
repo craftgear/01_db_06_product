909e77709eac77374704eec842541f8bbe94c029
Shunsuke Watanabe <craftgear@gmail.com>
2025-06-21 12:16:09 +0900

    SearchBox

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,24 @@
-'use client'
+import { useRef } from 'react';
 
-export default function SearchBar() {
+export default function SearchBar({ handleSearch }: { handleSearch?: (query: string) => void }) {
+  const ref = useRef<HTMLInputElement>(null);
+
+
+  const handleSubmit = () => {
+    if (!ref.current) return;
+    const inputValue = ref.current.value;
+    handleSearch?.(inputValue);
+  }
+
   return (
     <div className="join">
       <div>
         <label className="input validator join-item w-md">
-          <input className="w-full" type="email" placeholder="リポジトリ名を入力してください" required />
+          <input ref={ref} className="w-full" type="text" placeholder="リポジトリ名を入力してください" required />
         </label>
         <div className="validator-hint hidden">リポジトリ名が入力されていません</div>
       </div>
-      <button className="btn btn-neutral join-item">検索</button>
+      <button type="button" className="btn btn-neutral join-item" onClick={handleSubmit}>検索</button>
     </div>
-
   )
 }
